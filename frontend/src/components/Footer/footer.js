b96c573cc55958854635ec6077b754a477955cd2
esousasilva <esousasilva@seattleu.edu>
2025-08-04 12:56:51 -0700

    Created the footer section

---
+++ frontend/src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      Copyright {'\u00A9'} 2025 Eduardo Sousa. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer;
