e72b16d8ac516f9f67d88fdff6270efbf3b4feb8
esousasilva <esousasilva@seattleu.edu>
2025-08-04 12:54:42 -0700

    Added contact and footer section to the app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
 import Projects from "./components/Projects/projects";
+import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Intro/>
       <Skills/>
       <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
